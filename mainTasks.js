134cc32638d912b8fdd1588e3ab56bc1253445ee
Elen <elen.ghazaryan6@gmail.com>
2021-12-02 12:11:55 +0400

    feat: write Task4

--- mainTasks.js
+++ mainTasks.js
@@ -19,3 +19,11 @@ let b = 3;
 let c = a; // c = 10,
 a = b; // a = 3
 b = c; // b = 10
+
+//Task4
+let a = 10;
+let b = 3;
+
+a = a + b; // a = 13, b = 3
+b = a - b; // a = 13, b = 10
+a = a - b; // a = 3; b = 10
